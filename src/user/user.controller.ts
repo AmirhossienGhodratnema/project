bc1de5a4fefbdfb56175348e9a0698097d05c686
amirhossein <amirhosseinghodratnema@gmail.com>
2025-02-16 17:57:36 +0330

    Set pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 
@@ -17,7 +17,7 @@ export class UserController {
     };
 
     @Get('/:id')
-    findOne(@Param('id') id: number) {
+    findOne(@Param('id',ParseIntPipe) id: number) {
         const user = this.userService.findOne(+id);
         console.log("🚀 ~ UserController ~ findOne ~ user:", user)
         return user
